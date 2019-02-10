9cca00f7c4dc6b0f3641f2bfc8a84d78ba145f8b
rahulvijay11 <43632867+rahulvijay11@users.noreply.github.com>
2019-02-10 10:59:24 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    char ch,a,e,i,o,u,A,E,O,I,U;
+    scanf("%c",&ch);
+    if((ch>=65&&ch<=90)||(ch>97&&ch<=122))
+    {
+   switch(ch)
+   {
+      case 'a':
+      case 'A':
+      case 'e':
+      case 'E':
+      case 'o':
+      case 'O':
+      case 'U':
+      case 'u':
+      case 'i':
+      case 'I':
+      printf("vowels");
+      break;
+      default:
+      printf("Consonant");
+   }
+}
+    else
+    printf("invalid");
+    
+   return 0;
+}
+   
